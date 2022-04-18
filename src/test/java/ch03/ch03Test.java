93ba91f9314fe04214dd3d25581659943b26ee7c
kimJaeHyeon <kmr2644@gmail.com>
2022-04-18 12:04:24 +0900

    ch03

--- src/test/java/ch03/ch03Test.java
+++ src/test/java/ch03/ch03Test.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -30,7 +32,7 @@ class ch03Test {
     }
 
     @Test
-    public void 필터테스트() throws Exception{
+    public void Predicate_테스트() throws Exception{
         //given
         String str1 = "hello my world!";
         String[] split = str1.split("");
@@ -40,6 +42,35 @@ class ch03Test {
         //then
         assertEquals(result.stream().collect(Collectors.joining()),"hellomyworld!");
     }
+    @Test
+    public void Consumer_테스트() throws Exception{
+        //given
+        forEach(Arrays.asList(1, 2, 3, 4, 5), (Integer i) -> System.out.println("i = " + i));
+        //when
+        //then
+    }
+
+    @Test
+    public void Function_테스트() throws Exception{
+        //given
+        List<Integer> result = map(Arrays.asList(1, 2, 3, 4, 5, 6), (Integer i) -> i + 10);
+        //when
+        int sum = result.stream()
+                        .mapToInt(i -> i)
+                        .sum();
+        //then
+        assertEquals(sum,81);
+
+    }
+    public <T, R> List<R> map(List<T> list, Function<T, R>function) {
+        //T -> 매핑 전 타입
+        //R -> 매핑 후 타입
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            result.add(function.apply(t));
+        }
+        return result;
+    }
     public <T> List<T> filter(List<T> list, Predicate<T> predicate) {
         List<T> result = new ArrayList<>();
         for (T t : list) {
@@ -48,6 +79,12 @@ class ch03Test {
             }
         }
         return result;
+    }
+
+    public<T> void forEach(List<T> list, Consumer<T> consumer) {
+        for (T t : list) {
+            consumer.accept(t);
+        }
     }
     public String process(BufferedReaderProcessor p) throws IOException {
 
